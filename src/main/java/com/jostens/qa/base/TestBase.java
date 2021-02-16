d9fa5db05ba217f1ed9536f4b96493f63e3fc392
Steven VerMulm <SVerMulm@mycwt.com>
2021-02-16 00:44:08 -0600

    Feb 16th commit

--- src/main/java/com/jostens/qa/base/TestBase.java
+++ src/main/java/com/jostens/qa/base/TestBase.java
@@ -185,6 +185,12 @@ public class TestBase {
 	@AfterMethod
 	public void afterMethod(ITestResult result) throws IOException {
 		if(result.getStatus() == ITestResult.FAILURE) {
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
 			//Do not output to Excel File, if performing the @Test=aloginTest, since there is no excel file for it
 			if (!result.getName().equals("loginTest")) {
 				excelMethods.setDataTableCell("Failure - " + excelMethods.getCurrentDateTime(), iteration, column);
